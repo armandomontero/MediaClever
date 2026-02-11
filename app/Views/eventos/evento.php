fa83de0ded990f9af7adf45c73b36a7575c170dd
ArmandoDev <amonterof@gmail.com>
2026-02-10 21:03:26 -0300

    ahora por defecto marca espacio en agenda

--- app/Views/eventos/evento.php
+++ app/Views/eventos/evento.php
@@ -341,13 +341,13 @@
                         <div class="col-12 col-sm-3">
                             <label for="reservado">¿Reserva Espacio de Agenda? </label>
                             <div class="form-radio">
-                                <input class="form-radio-input" <?php if ($datos->reservado == 1) {
+                                <input class="form-radio-input" <?php if ($datos->reservado == 1 || $datos->state == 'Agendado') {
                                                                     echo 'checked';
                                                                 } ?> type="radio" required name="reservado" value="1" id="reservado1" />
                                 <label class="form-radio-label" for="reservado1">Si</label>
                             </div>
                             <div class="form-radio">
-                                <input class="form-radio-input" <?php if ($datos->reservado == 0) {
+                                <input class="form-radio-input" <?php if ($datos->reservado == 0 || $datos->state == 'Agendado') {
                                                                     echo 'checked';
                                                                 } ?> type="radio" name="reservado" value="0" id="reservado2" />
                                 <label class="form-radio-label" for="reservado2">No</label>
